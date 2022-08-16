5726f5e0fb05211e08e52502aca2fb4f0dc418d9
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2022-08-17 02:32:06 +0600

    Initial

---
+++ Console/InstallCommand.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Modules\Core\Console;
+
+use Artisan;
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Process\Process;
+
+class InstallCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'medicare:install {--demo}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Medicare Installer';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('Medicare Installing...');
+        if (!base_path('.env')){
+            $process = new Process(['cp .env.example .env']);
+            $process->run();
+        }
+
+        if ($this->option('demo')){
+            Artisan::call('migrate:fresh --seed');
+            Artisan::call('module:seed');
+        }else{
+            Artisan::call('migrate:fresh');
+        }
+
+        $this->info('Medicare Installed Successfully');
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['example', InputArgument::REQUIRED, 'An example argument.'],
+        ];
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null],
+        ];
+    }
+}
