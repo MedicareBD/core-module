c3a50d4f049664837c320d0821d0949a787b3903
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2022-08-18 11:57:57 +0600

    New Module
    Signed-off-by: Bishwajit Adhikary <bishwajitcadhikary@gmail.com>

--- Providers/CoreServiceProvider.php
+++ Providers/CoreServiceProvider.php
@@ -91,9 +91,9 @@ class CoreServiceProvider extends ServiceProvider
         $this->publishes([
             module_path($this->moduleName, 'Config/config.php') => config_path($this->moduleNameLower . '.php'),
         ], 'config');
-        $this->mergeConfigFrom(
-            module_path($this->moduleName, 'Config/config.php'), $this->moduleNameLower
-        );
+//        $this->mergeConfigFrom(
+//            module_path($this->moduleName, 'Config/config.php'), $this->moduleNameLower
+//        );
     }
 
     /**
